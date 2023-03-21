cc19d2e4fc0e53b33b79294da01144f949111ace
Emi-Bassy <ichimura.emi01@gmail.com>
2023-03-21 14:11:38 +0900

    データベースファイル新規作成

---
+++ Trash.js
@@ -0,0 +1,38 @@
+
+
+const allTrash = [
+    {
+        Number: "01",
+        Ward: "文京区",
+        Separation: ["資源", "プラスチック", "可燃", "不燃", "蛍光管"],
+        Category: ["食べ物", "アメニティ", "料理", "日用品"],
+        "Trash": [
+            {
+                Name: "新聞紙",
+                Type: "資源",
+                Category: "日用品"
+            },
+            {
+                Name: "袋類",
+                Type: "プラスチック",
+                Category: "食べ物"
+            },
+            {
+                Name: "ティッシュペーパー",
+                Type: "可燃",
+                Category: "日用品"
+            },
+            {
+                Name: "金属・刃物類",
+                Type: "不燃",
+                Category: "料理"
+            },
+            {
+                Name: "蛍光灯・電池",
+                Type: "蛍光管",
+                Category: "パーティ"
+            },
+        ]
+    }
+]
+
