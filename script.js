b71f1a4037967578cc5865450de0cae4bbca9ce0
sonoe <sonoe2000yamaguchi@gmail.com>
2023-03-21 11:31:00 +0900

    jsの動きを確認しました

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-$(function() {
-    $('.intro').addClass('go');
-  
-    $('.reload').click(function() {
-      $('.intro').removeClass('go').delay(200).queue(function(next) {
-        $('.intro').addClass('go');
-        next();
-      });
-  
-    });
-  })
+function changeColor(idname){
+    var obj = document.getElementById(idname);
+    obj.style.color = '#ffffff';            //文字色を白にする
+    obj.style.backgroundColor = '#ff0000';  //背景色を赤にする
+  }
